83b656427ebe03e858a8ab29086d01bb2d4919c6
bohong <bohongli2001@gmail.com>
2025-05-19 23:48:08 +0200

    removed the output line print

--- ready-for-it.py
+++ ready-for-it.py
@@ -2,5 +2,4 @@ def taylor_rerecords(title: str, album: str, year: int) -> str:
 
     return f"Taylor Swift rerecorded '{title}' from album {album} in year {year}."
 
-print(taylor_rerecords("... ready for it?", "reputation", 2025))
 
