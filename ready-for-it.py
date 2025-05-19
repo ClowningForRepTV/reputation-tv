5f89e90ede7e28ae4e4041fa35177d5dec235ced
bohong <bohongli2001@gmail.com>
2025-05-19 23:51:33 +0200

    converted the double quotation to single quotation in the output of function taylor_rerecords in ready-for-it.py

--- ready-for-it.py
+++ ready-for-it.py
@@ -1,5 +1,3 @@
 def taylor_rerecords(title: str, album: str, year: int) -> str:
 
-    return f"Taylor Swift rerecorded '{title}' from album {album} in year {year}."
-
-
+    return f'Taylor Swift rerecorded \'{title}\' from album {album} in year {year}.'
