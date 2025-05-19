58aff8713e8cfe8df24c8f2f6db0906add678c77
bohong <bohongli2001@gmail.com>
2025-05-20 00:26:09 +0200

    inserted an if flow to detect if the input album is an album that has not been rerecorded yet

--- ready-for-it.py
+++ ready-for-it.py
@@ -1,4 +1,8 @@
 def taylor_rerecords(title: str, album: str, year: int) -> str:
     """
 
+    :type album: str
+    """
+    if album == "reputation" or album == "Taylor Swift":
+        return f"{album} is still being clowned"
     return f'Taylor Swift rerecorded \'{title}\' from album {album} in year {year}.'
